e1df4a175584cc39b319733b0e6b3f8d17acd675
mikibonacci <mikibonacci@hotmail.it>
2024-12-10 09:00:09 +0000

    Adding undi_analysis and examples

---
+++ examples/full_undi_analysis.py
@@ -0,0 +1,11 @@
+from undi.undi_analysis import execute_undi_analysis
+from ase.io import read 
+
+structure = read('Cu6H.cif')
+
+# to dump into results.json: res = execute_undi_analysis(structure,max_hdim=1e3, dump=True)
+# else
+res = execute_undi_analysis(structure,max_hdim=1e3)
+
+# to do the same but from cmd line: python -m undi Cu6H.cif --dump True
+# this will use the __main__.py
