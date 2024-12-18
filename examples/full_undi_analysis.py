698ddb199f30939404a2d129b6f9ceb9733df054
Pietro Bonfa <bonfus@gmail.com>
2024-12-18 13:22:43 +0100

    Increased minimum H space dimension

--- examples/full_undi_analysis.py
+++ examples/full_undi_analysis.py
@@ -5,7 +5,7 @@ structure = read('Cu6H.cif')
 
 # to dump into results.json: res = execute_undi_analysis(structure,max_hdim=1e3, dump=True)
 # else
-res = execute_undi_analysis(structure,max_hdim=1e3)
+res = execute_undi_analysis(structure,max_hdim=1e4)
 
 # to do the same but from cmd line: python -m undi Cu6H.cif --dump True
 # this will use the __main__.py
