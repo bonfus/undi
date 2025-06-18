ccc34d6c9f559da1870fb5062c2a3dc3594840f2
Pietro Bonfa <bonfus@gmail.com>
2025-06-18 18:21:55 +0200

    Added fibonacci sphere method to generate almost uniform points on a sphere

--- undi/auto_analysis.py
+++ undi/auto_analysis.py
@@ -71,6 +71,24 @@ def gen_neighbouring_atomic_structure(atoms, isotopes, spins, hdim_max):
 
     return data, hdim
 
+def fibonacci_sphere(samples=100):
+
+    points = []
+    phi = np.pi * (np.sqrt(5.) - 1.)  # golden angle in radians
+
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = np.sqrt(1 - y * y)  # radius at y
+
+        theta = phi * i  # golden angle increment
+
+        x = np.cos(theta) * radius
+        z = np.sin(theta) * radius
+
+        points.append((x, y, z))
+
+    return points
+
 def execute_auto_analysis(
         structure: Atoms,
         B_mod: float = 0.0,
